501a6f086d4fcdfcd0d5b5bf033e78201d4967a0
juanfstillo <juan.stillo1@gmail.com>
2021-03-03 19:30:49 -0300

    Iniciando rep

---
+++ js/index.js
@@ -0,0 +1,38 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var $title = $('.js-title');
+var copy   = '.js-copy';
+
+$title.click(function () {
+$(this).next(copy).slideToggle();
+$(this).parent().siblings().children().next().slideUp();
+return false;
+});
+
